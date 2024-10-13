0f87cc286ac7e7d044d83fb1c44d1ee313089e1c
Pedro <alejy1@gmail.com>
2024-10-13 17:35:11 -0300

    añadir foother

--- src/renderer/shared/Footer.tsx
+++ src/renderer/shared/Footer.tsx
@@ -1,17 +1,18 @@
 "use client";
-import LogoFlor from "@/../public/loto-flor.png";
-import Facebook from "@/../public/facebook.png";
-import Twitter from "@/../public/twitter.png";
-import Instagram from "@/../public/instagram.png";
-import Github from "@/../public/github.png";
-import Image from "next/image";
-import Link from "next/link";
+import LogoFlor from "@/src/renderer/public/img/loto-flor.png";
+import Facebook from "@/src/renderer/public/img/facebook.png";
+import Twitter from "@/src/renderer/public/img/twitter.png";
+import Instagram from "@/src/renderer/public/img/instagram.png";
+import Github from "@/src/renderer/public/img/github.png";
+
 import { useEffect, useState } from "react";
-import { Service } from "@/interfaces/Service.interface";
-import { serviceGetAllServices } from "@/services/services.service";
-import LoadingSpinner from "../LoadingSpinner/LoadingSpinner";
-import { useAuthContext } from "@/Context/AuthContext";
-import { useToast } from "@/hooks/use-toast";
+
+import { Link } from "react-router-dom";
+import LoadingSpinner from "./LoadingSpinner";
+import { Service } from "../interfaces/Service.interface";
+import { serviceGetAllServices } from "../services/services.service";
+import { useAuthContext } from "../Context/AuthContext";
+import { useToast } from "./hooks/use-toast";
 
 const Footer = () => {
   
@@ -42,27 +43,27 @@ const Footer = () => {
   return (
     <div className="shadow-sm flex flex-col md:flex-row items-start md:items-center justify-evenly py-4 gap-x-4 h-[300px]">
       <div className="hidden lg:flex flex-col p-10 justify-start items-center">
-        <Image src={LogoFlor} alt="Logo SPA Sentirse bien" width={"100"} />
+        <img src={LogoFlor} alt="Logo SPA Sentirse bien" width={"100"} />
       </div>
       <div className="flex-col flex p-10 justify-start  items-start h-full">
         <h3 className="pb-5 text-lg font-bold text-gray-600">Contactanos</h3>
         <p className="font-light text-md text-gray-500">Av. Sarmiento 55</p>
         <p className="font-light text-md text-gray-500">Resistencia - Chaco</p>
-        <Link target="blank" href={`mailto:spasentirsebien@spa.com.ar?subject=${encodeURIComponent("Consulta")}&body=${encodeURIComponent("¡Hola! Quiero realizar una consulta.")}`} className="font-light hover:underline text-md text-gray-500">Email: spasentirsebien@spa.com.ar</Link>
-        <Link target="blank" href="https://wa.me/5493624000000?text=Bienvenido" className="font-light text-md hover:underline text-gray-500">Whatsapp: 3624000000</Link>
+        <Link target="blank" to={`mailto:spasentirsebien@spa.com.ar?subject=${encodeURIComponent("Consulta")}&body=${encodeURIComponent("¡Hola! Quiero realizar una consulta.")}`} className="font-light hover:underline text-md text-gray-500">Email: spasentirsebien@spa.com.ar</Link>
+        <Link target="blank" to="https://wa.me/5493624000000?text=Bienvenido" className="font-light text-md hover:underline text-gray-500">Whatsapp: 3624000000</Link>
         <div className="flex justify-start w-full gap-4 pt-5">
-          <a href="https://facebook.com"><Image src={Facebook} alt="Logo facebook"    width="40" /></a>
-          <a href="https://x.com"><Image src={Twitter} alt="Logo Twitter"             width="40" /></a>
-          <a href="https://github.com"><Image src={Github} alt="Logo Github"          width="40" /></a>
-          <a href="https://instagram.com"><Image src={Instagram} alt="Logo Instagram" width="40" /></a>
+          <a href="https://facebook.com"><img src={Facebook} alt="Logo facebook"    width="40" /></a>
+          <a href="https://x.com"><img src={Twitter} alt="Logo Twitter"             width="40" /></a>
+          <a href="https://github.com"><img src={Github} alt="Logo Github"          width="40" /></a>
+          <a href="https://instagram.com"><img src={Instagram} alt="Logo Instagram" width="40" /></a>
         </div>
       </div>
       <div className="flex-col text-left flex p-10 justify-start items-start h-full">
         <h3 className="pb-5 text-lg font-bold text-slate-600">Enlaces Rápidos</h3>
-        <Link href={"/"}          className="hover:underline font-light text-md text-gray-500">Home</Link>
-        <Link href={"/noticias"}  className="hover:underline font-light text-md text-gray-500">Noticias</Link>
-        <Link href={"/servicios"} className="hover:underline font-light text-md text-gray-500">Servicios</Link>
-        <Link href={`${!authUser ? "/auth/login" : "/turnos"}`}    className="hover:underline font-light text-md text-gray-500">Reservar turnos</Link>
+        <Link to={"/"}          className="hover:underline font-light text-md text-gray-500">Home</Link>
+        <Link to={"/noticias"}  className="hover:underline font-light text-md text-gray-500">Noticias</Link>
+        <Link to={"/servicios"} className="hover:underline font-light text-md text-gray-500">Servicios</Link>
+        <Link to={`${!authUser ? "/auth/login" : "/turnos"}`}    className="hover:underline font-light text-md text-gray-500">Reservar turnos</Link>
       </div>
       <div className="hidden md:flex w-1/3 flex-col p-10 text-left justify-start items-start h-full">
         <h3 className="pl-2 pb-5 text-lg font-bold text-slate-600">Imágenes</h3>
--- src/renderer/shared/Layout.tsx
+++ src/renderer/shared/Layout.tsx
@@ -1,12 +1,19 @@
 import { Outlet } from 'react-router-dom';
 import { Suspense } from 'react';
 import Navbar from './Navbar';
+import Footer from './Footer';
 
 function Layout() {
   return (
     <div className="">
       <Navbar />
+    
       <Outlet />
+
+      <Footer />
+
+
+
     </div>
   );
 }
